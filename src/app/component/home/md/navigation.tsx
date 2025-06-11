13227f96dba574f4c8fad0353d638263e90dfec2
Reymark <genuinepower123@gmail.com>
2025-06-11 19:55:15 +0800

    alls

--- src/app/component/home/md/navigation.tsx
+++ src/app/component/home/md/navigation.tsx
@@ -9,8 +9,12 @@ const dispatch = useDispatch();
   return (
     <div className='h-[300px] bg-[#94A6FF] pt-24 flex justify-evenly'>
         <Link href="reclamation" className=' h-[140px] w-[200px] bg-[#D9D9D9] flex items-center justify-center rounded-xl shadow-[4px_4px_4px_rgba(0,0,0,.5)] cursor-pointer font-poppins'>reclamation</Link>
-        <button onClick={()=>dispatch(setTodaysCoding(true))} className=' h-[140px] w-[200px] bg-[#D9D9D9] rounded-xl shadow-[4px_4px_4px_rgba(0,0,0,.5)] cursor-pointer font-poppins'>today's coding</button>
-        <button onClick={()=>dispatch(setTrafficRoads(true))} className=' h-[140px] w-[200px] bg-[#D9D9D9] rounded-xl shadow-[4px_4px_4px_rgba(0,0,0,.5)] cursor-pointer font-poppins'>traffic roads</button>
+        <button 
+          onClick={()=>dispatch(setTodaysCoding(true))} 
+          className='h-[140px] w-[200px] bg-[#D9D9D9] rounded-xl shadow-[4px_4px_4px_rgba(0,0,0,.5)] cursor-pointer font-poppins'>today's coding</button>
+        <button 
+          onClick={()=>dispatch(setTrafficRoads(true))} 
+          className='h-[140px] w-[200px] bg-[#D9D9D9] rounded-xl shadow-[4px_4px_4px_rgba(0,0,0,.5)] cursor-pointer font-poppins'>traffic roads</button>
     </div>
   );
 };
